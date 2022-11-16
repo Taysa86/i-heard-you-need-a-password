c6ccee941b6c74d1ea98e593a43318f23814ec5f
Taysa86 <Taysarolu86@gmail.com>
2022-11-16 15:32:58 -0600

    added loop to generate password funtion

--- assets/script.js
+++ assets/script.js
@@ -1,23 +1,25 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
-// Arrays
+// strings
   var letters = "abcdefghijklmnopqrstuvwxyz";
   var caps = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
   var numbers = "0123456789";
   var special = "!@#$%^&*()";
 
- // Variables
+// Variables
   var wantlowercase;
   var wantuppercase; 
   var wantnumbers ;
   var wantspecial ;
   var wantlength = "";
 
-  function getRandomArbitrary (max) {
+// Random math function
+function getRandomArbitrary (max) {
     return Math.random() * (max);
   }
 
+//generate password function
 function generatePassword() {
   let letter = letters.charAt(0)
   console.log (letter)
@@ -41,11 +43,21 @@ function generatePassword() {
   }
     let index = getRandomArbitrary(selectedcharacters.length)
   console.log(selectedcharacters.charAt(index))
+// Variable for loop
+  var ranpass = ""
+// Loop for generatepassword function
+  for (var i = 0; i < wantlength; i++) {
+    ranpass = ranpass + selectedcharacters[Math.floor(Math.random() * selectedcharacters.length)];
+    console.log(ranpass)
+  }
+  return ranpass;
 } 
 
 // Write password to the #password input
 function writePassword() {
   wantlength = (prompt ("Pick a length between 8 and 128 characters."));
+
+//should place alert here incase they select a length outside of 8 & 128, will have to fix later
 
 // Prompts for parameters
   wantlowercase = confirm("Would you like lowercase letters?");
@@ -53,6 +65,7 @@ function writePassword() {
   wantnumbers = confirm("Would you like numbers?");
   wantspecial = confirm("would you like special characters?");
   console.log (wantlowercase,wantuppercase,wantnumbers,wantspecial)
+  
   var password = generatePassword()
 
   ;
